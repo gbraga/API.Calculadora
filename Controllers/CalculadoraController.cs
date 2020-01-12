b10be75ce8ea1e13c5bb239b29be7cffad4179cf
Gabriel Braga <gbraga.dev@gmail.com>
2020-01-11 22:59:58 -0300

    v0.0.1

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Calculardora.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Calculardora.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CalculadoraController : ControllerBase
+    {
+        [HttpPost]
+        [Route("somar")]
+        public double Somar(Operacao op) => op.valor1 + op.valor2;
+
+        [HttpPost]
+        [Route("subtrair")]
+        public double Subtrair(Operacao op) => op.valor1 - op.valor2;
+
+        [HttpPost]
+        [Route("multiplicar")]
+        public double Multiplicar(Operacao op) => op.valor1 * op.valor2;
+
+        [HttpPost]
+        [Route("dividir")]
+        public double Dividir(Operacao op) => op.valor1 / op.valor2;
+    }
+}
---
+++ Models/Operacao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Calculardora.Models
+{
+    public class Operacao
+    {
+        public double valor1 { get; set; }
+        public double valor2 { get; set; }
+    }
+}
